06158c5dabbe958432db150475363ff65e30399f
Serfiden <cristi.silav@gmail.com>
2017-10-29 21:27:02 +0200

    Added comments

--- Greedy/Minimum Decreasing Subsets (grading)/newInput.cpp
+++ Greedy/Minimum Decreasing Subsets (grading)/newInput.cpp
@@ -27,29 +27,33 @@ int displayStack (stack<int> subset){
 
 int customBinarySearch (vector < stack<int> > &set, int valueToPut, int limit){
 
+	/*  În esență, ce caut este o poziție i, astfel încât set[i] > valueToPut &&
+	valueToPut > set[i - 1] <=> cel mai mic vf. de stivă, mai mare decât valueToPut */
+
 	int left = 0;
 	int right = limit - 1;
 	int mid;
 
-	if (valueToPut > set[right].top()) return -1;
+	if (valueToPut > set[right].top()) return -1;			// verific dacă elementul verificat 
+															// trebuie să fie vârf de stivă nouă
 
-	else if (valueToPut <= set[left].top()) return 0;
+	else if (valueToPut <= set[left].top()) return 0;		// încep căutarea din stânga
 
 	else while (left != right){	
 
 		mid = (left + right) / 2;
 
-		if (valueToPut == set[mid].top()) return mid;
+		if (valueToPut == set[mid].top()) return mid;	
 
-		else if (valueToPut < set[mid].top())
+		else if (valueToPut < set[mid].top())					// verific dacă poziția curentă este bună
 			if (valueToPut > set[mid - 1].top()) return mid;
 			else right = mid;
 
-		else if (valueToPut > set[mid].top())
+		else if (valueToPut > set[mid].top())					 // idem mai sus
 			if (valueToPut < set[mid + 1].top()) return mid + 1;
 			else left = mid;
 
-		if (valueToPut > set[right].top()) return -1;
+		if (valueToPut > set[right].top()) return -1;			// verific din nou capetele selecției
 
 		else if (valueToPut <= set[left].top()) return left;
 	}
@@ -63,10 +67,12 @@ int minNumberSubsets (vector < stack<int> > &initial){
 	vector< stack<int> >::iterator it = initial.begin();
 	int numberIterator = 0;
 
-	while (it + 1 != initial.end() && it != initial.end()){
+	while (it + 1 != initial.end() && it != initial.end()){		// iterez o singură dată prin șirul inițial
 		int modified = 0;
 
-		int foundPlace = customBinarySearch(initial, (*(it + 1)).top(), numberIterator + 1);
+		int foundPlace = customBinarySearch(initial, (*(it + 1)).top(), numberIterator + 1); 
+
+		// dacă pot insera elementul curent într-o stivă anterioară
 
 		if (foundPlace >= 0){
 			
@@ -76,6 +82,8 @@ int minNumberSubsets (vector < stack<int> > &initial){
 			initial[foundPlace].push(toAdd);
 			modified = 1;
 		} 
+
+		// dacă nu am făcut nicio inserție/ștergere, atunci avansez cu verificarea în vector; altfel, staționez
 
 		if (it + 1 != initial.end() && !modified){
 			it ++;
